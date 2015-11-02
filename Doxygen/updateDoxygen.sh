5fe2bbbd12cb30c9e99251f5a86fba6a20b4a4bb
Ivan Dardi <ivy.dardi@gmail.com>
2015-11-02 15:05:42 -0200

    Added a script to automatically update the Doxygen

---
+++ Doxygen/updateDoxygen.sh
@@ -0,0 +1,16 @@
+# Generate Doxygen
+doxygen doxygen
+
+# Change into the gh-pages branch
+cd html
+
+# Committing and pushing the new Doxygen
+git add .
+git commit -m "Updating Doxygen."
+git push -u origin gh-pages
+
+# Committing the changes to the master branch
+cd ../..
+add .
+commit -m "Committing Doxygen update to master branch."
+push -u origin master
